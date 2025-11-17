0b875bf507e419955dc9500e42e367f38811d37c
Luke T. Shumaker <lukeshu@parabola.nu>
2025-11-17 15:44:55 -0700

    Fix empty entries in mime.types.nginx
    There are a few lines in mime.types that have no file extension, but
    have trailing whitespace at the end.  The intent of the final regex in
    the `sed` command in generate-nginx-mimetypes.sh is clearly to only
    match lines that have file extensions; but for the described lines has
    a false-positive, mistaking the inserted trailing-";" for a file
    extension.
    
    Fix this by changing the file-extension-detecting part of the regex
    from
    
        [^[:space:]]
    
    to
    
        [^[:space:];]

--- generate-nginx-mimetypes.sh
+++ generate-nginx-mimetypes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "types {"
-sed -e 's/#.*//' -e 's/$/;/' -ne '/[^[:space:]][[:space:]]\{1,\}[^[:space:]]/p'
+sed -e 's/#.*//' -e 's/$/;/' -ne '/[^[:space:]][[:space:]]\{1,\}[^[:space:];]/p'
 echo "}"
